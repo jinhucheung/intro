86dbf146d87ba5865da0948123e02734fac50a94
jimcheung <hi.jinhu.zhang@gmail.com>
2019-07-18 21:22:23 +0800

    update tour form

--- app/helpers/intro/application_helper.rb
+++ app/helpers/intro/application_helper.rb
@@ -3,5 +3,18 @@ module Intro
     def sign_out_admin_intro_path
       Intro.config.sign_out_admin_path.presence || sign_out_admin_sessions_path
     end
+
+    def intro_tag
+      return unless enable_intro?
+
+      <<-HTML
+        #{javascript_include_tag('intro/application')}
+        #{stylesheet_link_tag('intro/application')}
+      HTML
+    end
+
+    def enable_intro?
+      Intro.config.enable && request.get? && !request.xhr? && public_send(Intro.config.current_user_method)
+    end
   end
 end
